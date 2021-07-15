0d57aaec761ef07b705cccc1e024bd03fded89bd
TK-Khaw <t.kang_khaw@hotmail.com>
2021-07-15 11:26:56 +0800

    Added pattern of commit messages to listen for.

--- trigger.sh
+++ trigger.sh
@@ -9,7 +9,19 @@ then
 	git pull
 	MSG=$(git log -1 --pretty="format:$COMMIT_FORMAT")
 	cd $JKJAV_AUTO_DIR
-	$PYTHON3 $AUTONOT_SCRIPT "$MSG"
+
+	while read rgx
+	do
+		BUF=$(echo $MSG | grep -E "^$rgx$" )
+		if [[ $BUF != '' ]]; then break; fi
+	done < wl_msg_ptn
+
+	if [[ $BUF != '' ]]
+	then
+		$PYTHON3 $AUTONOT_SCRIPT "$MSG"
+	else
+		echo 'Not related'
+	fi
 else
 	echo 'Update to date!'
 fi
